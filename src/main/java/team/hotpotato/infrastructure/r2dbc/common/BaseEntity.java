2a25fbfea0f576a72eded9a5c8f9afada034f0b7
Hyunwoo Gu <realization0327@gmail.com>
2026-03-26 12:27:18 +0900

    refactor: BaseEntity 수정

--- src/main/java/team/hotpotato/infrastructure/r2dbc/common/BaseEntity.java
+++ src/main/java/team/hotpotato/infrastructure/r2dbc/common/BaseEntity.java
@@ -1,10 +1,13 @@
 package team.hotpotato.infrastructure.r2dbc.common;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.experimental.SuperBuilder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.LocalDateTime;
@@ -17,15 +20,33 @@ public class BaseEntity {
     @Column("id")
     private Long id;
 
+	@LastModifiedDate
     @Column("updatedAt")
     private LocalDateTime updatedAt;
 
+	@CreatedDate
     @Column("createdAt")
     private LocalDateTime createdAt;
 
+	@Builder.Default
     @Column("deleted")
-    private Boolean deleted;
+    private Boolean deleted = false;
 
     @Column("deleted_at")
     private LocalDateTime deletedAt;
+
+	public void delete() {
+		this.deleted = true;
+		this.deletedAt = LocalDateTime.now();
+	}
+
+	public void restore() {
+		this.deleted = false;
+		this.deletedAt = null;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
 }
+
