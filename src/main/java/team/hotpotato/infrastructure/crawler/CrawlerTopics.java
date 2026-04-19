0b9787ce9803d2c170bcfa39c2bbdec72d0ebc8b
Hyunwoo Gu <realization0327@gmail.com>
2026-04-20 08:16:27 +0900

    fix(crawler): 커뮤니티 출력 토픽명 변경

--- src/main/java/team/hotpotato/infrastructure/crawler/CrawlerTopics.java
+++ src/main/java/team/hotpotato/infrastructure/crawler/CrawlerTopics.java
@@ -8,6 +8,6 @@ public final class CrawlerTopics {
 
     public static final String CRAWL_REQUEST = "crawl.request";
     public static final String CRAWL_RESULT = "crawl.community.result";
-    public static final String CRAWL_POST_DEDUPED = "crawl.post.deduped";
-    public static final String CRAWL_COMMENT_DEDUPED = "crawl.comment.deduped";
+    public static final String CRAWL_POST_DEDUPED = "crawl.community.post.deduped";
+    public static final String CRAWL_COMMENT_DEDUPED = "crawl.community.comment.deduped";
 }
