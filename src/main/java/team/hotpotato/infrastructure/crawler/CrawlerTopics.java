3650ad7e15a8e0459971a70370cc0419ac1ee86f
Hyunwoo Gu <realization0327@gmail.com>
2026-04-20 08:13:06 +0900

    fix(crawler): 결과 토픽명 변경

--- src/main/java/team/hotpotato/infrastructure/crawler/CrawlerTopics.java
+++ src/main/java/team/hotpotato/infrastructure/crawler/CrawlerTopics.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 public final class CrawlerTopics {
 
     public static final String CRAWL_REQUEST = "crawl.request";
-    public static final String CRAWL_RESULT = "crawl.result";
+    public static final String CRAWL_RESULT = "crawl.community.result";
     public static final String CRAWL_POST_DEDUPED = "crawl.post.deduped";
     public static final String CRAWL_COMMENT_DEDUPED = "crawl.comment.deduped";
 }
